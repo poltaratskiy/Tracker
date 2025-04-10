5024714a497d59a5d987b7a0ee5b8ac91e5c6737
Konstantin Poltaratskii <k.poltarackiy@gmail.com>
2025-04-11 00:16:50 +0200

    Logging, seeding data, migrations

--- src/Tracker.Dotnet.Auth/Program.cs
+++ src/Tracker.Dotnet.Auth/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Identity;
+п»їusing Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
@@ -14,95 +13,122 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-#region loggerconfig
-var loggerConfig = new Action<LoggerConfiguration>((loggerConfiguration) =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
 {
-    var appName = Assembly.GetCallingAssembly().GetName().Name;
-    var env = builder.Environment;
-    var appEnvironment = env.IsDevelopment() ? "Development" : "Production";
+    var assemblyName = Assembly.GetCallingAssembly().GetName().Name;
+    Log.Information($"Starting {assemblyName} application...");
 
-    loggerConfiguration
-        .MinimumLevel.Debug()
-        .MinimumLevel.Override("System", LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft", env.IsDevelopment() ? LogEventLevel.Information : LogEventLevel.Warning)
-        .Enrich.FromLogContext()
-        .Enrich.WithProperty("Application", appName)
-        .Enrich.WithProperty("ApplicationEnvironment", appEnvironment)
-        .Enrich.WithExceptionDetails()
-        .ReadFrom.Configuration(configuration, new ConfigurationReaderOptions
+    // Add services to the container.
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(configuration.GetConnectionString("ApplicationDbContext")));
+
+    builder.Services.AddIdentity<User, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    services.AddSerilog(loggerConfiguration =>
+    {
+        var appName = Assembly.GetCallingAssembly().GetName().Name;
+        var env = builder.Environment;
+        var appEnvironment = env.IsDevelopment() ? "Development" : "Production";
+
+        loggerConfiguration
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft", env.IsDevelopment() ? LogEventLevel.Information : LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Application", appName)
+            .Enrich.WithProperty("ApplicationEnvironment", appEnvironment)
+            .Enrich.WithExceptionDetails()
+            .ReadFrom.Configuration(configuration, new ConfigurationReaderOptions
+            {
+                SectionName = "Logging",
+            });
+
+        //elastic or something similar
+
+        if (env.IsDevelopment())
         {
-            SectionName = "Logging",
-        });
+            loggerConfiguration.WriteTo.Console(
+                restrictedToMinimumLevel: LogEventLevel.Debug,
+                outputTemplate: "{Timestamp:O} [{Level:u3}] [{SourceContext}] {Scope:lj} {Message:lj}{NewLine}{Exception}");
+        }
+        else
+        {
+            loggerConfiguration.WriteTo.Console(
+                formatter: new Serilog.Formatting.Compact.RenderedCompactJsonFormatter(),
+                restrictedToMinimumLevel: LogEventLevel.Information);
+        }
+    });
 
-    //elastic or something similar
+    var app = builder.Build();
+    Log.Information($"Configuring services at {assemblyName} has been finished");
 
-    if (env.IsDevelopment())
+    using (var scope = app.Services.CreateScope())
     {
-        loggerConfiguration.WriteTo.Console(
-            restrictedToMinimumLevel: LogEventLevel.Debug,
-            outputTemplate: "{Timestamp:O} [{Level:u3}] [{SourceContext}] {Scope:lj} {Message:lj}{NewLine}{Exception}");
+        var scopeProvider = scope.ServiceProvider;
+        var logger = scopeProvider.GetRequiredService<ILogger<Program>>();
+
+        try
+        {
+            logger.LogInformation("Starting Postgres DB migration...");
+            var db = scopeProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+
+            logger.LogInformation("DB migration successfully finished. Start seeding test data...");
+            await SeedData.AddRoles(scopeProvider);
+            logger.LogInformation("Seeding test data successfully finished.");
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error occured while migrating db, application won't be started");
+            throw;
+        }
     }
-    else
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsProduction())
     {
-        loggerConfiguration.WriteTo.Console(
-            formatter: new Serilog.Formatting.Compact.RenderedCompactJsonFormatter(),
-            restrictedToMinimumLevel: LogEventLevel.Information);
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
-});
-#endregion loggerconfig
 
-// Add services to the container.
-services.AddSerilog(loggerConfig);
+    app.MapControllers();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(configuration.GetConnectionString("ApplicationDbContext")));
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application Tracker.Dotnet.Auth terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
-builder.Services.AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
 
-/*builder.Services.AddAuthentication(o => для сервиса где нужна авторизация
+
+/*builder.Services.AddAuthentication(o => РґР»СЏ СЃРµСЂРІРёСЃР° РіРґРµ РЅСѓР¶РЅР° Р°РІС‚РѕСЂРёР·Р°С†РёСЏ
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).
     .AddJwtBearer(...);
-builder.Services.AddAuthorization(); // потом посмотреть подробнее
+builder.Services.AddAuthorization(); // РїРѕС‚РѕРј РїРѕСЃРјРѕС‚СЂРµС‚СЊ РїРѕРґСЂРѕР±РЅРµРµ
 */
 
-services.AddControllers();
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
 
 
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var scopeProvider = scope.ServiceProvider;
-    var logger = scopeProvider.GetRequiredService<ILogger<Program>>();
-
-    try
-    {
-        logger.LogInformation("Starting Postgres DB migration...");
-        var db = scopeProvider.GetRequiredService<ApplicationDbContext>();
-        db.Database.Migrate();
-        logger.LogInformation("DB migration successfully finished");
-    }
-    catch (Exception e)
-    {
-        logger.LogError(e, "Error occured while migrating db, application won't be started");
-        throw;
-    }
-}
 
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 /* app.UseHttpsRedirection();
  * Disabled https only for this pet project to avoid issues with certificate installations.
@@ -110,11 +136,9 @@ if (!app.Environment.IsProduction())
  * MUST use https on production environment
  */
 
-/*app.UseAuthentication(); понадобятся в обычных сервисах
+/*app.UseAuthentication(); РїРѕРЅР°РґРѕР±СЏС‚СЃСЏ РІ РѕР±С‹С‡РЅС‹С… СЃРµСЂРІРёСЃР°С…
 app.UseAuthorization();*/
 
-app.MapControllers();
 
-app.Run();
 
 
