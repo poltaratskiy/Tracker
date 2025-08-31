3a203d83ea864d18d909904b39e64dca223bf062
Konstantin Poltaratskii <k.poltarackiy@gmail.com>
2025-08-31 21:16:16 +0200

    Added keycloak

---
+++ src/Tracker.Dotnet.SSO.Api/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OpenIddict.EntityFrameworkCore.Models;
+using Tracker.Dotnet.SSO.Api.Domain;
+
+namespace Tracker.Dotnet.SSO.Api.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<OpenIddictEntityFrameworkCoreApplication> Applications { get; set; } = default!;
+    public DbSet<OpenIddictEntityFrameworkCoreAuthorization> Authorizations { get; set; } = default!;
+    public DbSet<OpenIddictEntityFrameworkCoreScope> Scopes { get; set; } = default!;
+    public DbSet<OpenIddictEntityFrameworkCoreToken> Tokens { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.UseOpenIddict(); // adds schemas of OpenIddict
+    }
+}
